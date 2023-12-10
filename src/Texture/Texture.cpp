e1b4c4885a7f57624a23e3c130b99b0c20538703
Sovereign Shahid <sovereignshahid@gmail.com>
2023-12-09 17:18:58 -0700

    changed clamping

--- src/Texture/Texture.cpp
+++ src/Texture/Texture.cpp
@@ -14,8 +14,8 @@ Texture::Texture(const std::string& path)
     // SPECIFY THESE OR ELSE YOU GET A BLACK TEXTURE!!!!
     GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST));
     GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
-    GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER));
-    GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER));
+    GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
+    GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
 
     if (m_LocalBuffer)
     {
